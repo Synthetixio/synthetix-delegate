e71bf2d94d95000a4c5d44f0ce41fbc69697c589
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2020-06-07 11:36:04 +1000

    Add type and state to transaction The transaction saga should complete transaction rather than removing it.

--- src/store/ducks/transaction/actionTransactions.ts
+++ src/store/ducks/transaction/actionTransactions.ts
@@ -3,9 +3,12 @@ import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'store/types';
 import snxJSConnector from 'utils/snxJSConnector';
 import { fetchDelegateWalletInfoRequest } from '../delegates/delegateWalletInfo';
+import orderBy from 'lodash/orderBy';
 
 export interface Transaction {
 	hash: string;
+	type: 'burn' | 'mint' | 'claim';
+	state: 'pending' | 'complete';
 }
 export interface TransactionError {
 	errorMessageKey: string;
@@ -32,18 +35,18 @@ export const actionTransactionsSlice = createSlice({
 			state,
 			action: PayloadAction<{
 				hash: string;
+				type: Transaction['type'];
 				walletAddress: string;
 			}>
 		) => {
-			const { hash } = action.payload;
-			state.byHash[hash] = { hash };
+			const { hash, type } = action.payload;
+			state.byHash[hash] = { hash, type, state: 'pending' };
 		},
-		removeTransaction: (
-			state,
-			action: PayloadAction<{
-				hash: string;
-			}>
-		) => {
+		completeTransaction: (state, action: PayloadAction<{ hash: string }>) => {
+			const { hash } = action.payload;
+			state.byHash[hash].state = 'complete';
+		},
+		removeTransaction: (state, action: PayloadAction<{ hash: string }>) => {
 			const { hash } = action.payload;
 			delete state.byHash[hash];
 		},
@@ -66,17 +69,27 @@ export const actionTransactionsSlice = createSlice({
 export const {
 	addTransaction,
 	removeTransaction,
+	completeTransaction,
 	addError,
 	removeError,
 } = actionTransactionsSlice.actions;
 
 const getActionTransactionState = (state: RootState) => state.transaction[sliceName];
 
-export const getTransactions = createSelector(getActionTransactionState, transactionState =>
-	Object.values(transactionState.byHash)
+const getTransactions = createSelector(getActionTransactionState, (transactionState) =>
+	orderBy(Object.values(transactionState.byHash), (t) => t.state === 'pending', 'desc')
+);
+export const getClaimTransaction = createSelector(getTransactions, (transactions) =>
+	transactions.find((t) => t.type === 'claim')
+);
+export const getBurnTransaction = createSelector(getTransactions, (transactions) =>
+	transactions.find((t) => t.type === 'burn')
+);
+export const getMintTransaction = createSelector(getTransactions, (transactions) =>
+	transactions.find((t) => t.type === 'mint')
 );
 
-export const getErrors = createSelector(getActionTransactionState, transactionState =>
+export const getErrors = createSelector(getActionTransactionState, (transactionState) =>
 	Object.values(transactionState.errorsById)
 );
 
@@ -89,7 +102,7 @@ function* addTransactionSaga(action: PayloadAction<{ hash: string; walletAddress
 		yield put(removeError({ id: hash }));
 		yield put(fetchDelegateWalletInfoRequest({ walletAddresses: [walletAddress] }));
 	}
-	yield put(removeTransaction({ hash }));
+	yield put(completeTransaction({ hash }));
 }
 
 export function* watchAddTransaction() {
